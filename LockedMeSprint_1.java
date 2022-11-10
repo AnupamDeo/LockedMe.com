2a9bb2523179bd10afa87a492824f78dd8a58112
AnupamDeo <108815055+AnupamDeo@users.noreply.github.com>
2022-11-11 00:29:25 +0530

    Add files via upload

---
+++ LockedMeSprint_1.java
@@ -0,0 +1,194 @@
+
+public class LockedMeSprint_1 {
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * Name: Anupam Deo
+ * Date: - 26/10/2022
+ */
+public class App {
+	
+	public static boolean matchFlag = false;
+
+	static String MY_DIR = "C:/Users/Anupam/ForLockMe";
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		grabPath();
+		showWelcomeScreen();
+		showMainMenu();
+	}
+	
+	private static void grabPath() {
+		System.out.println("\nPlease specify a directory to use, or press [ENTER] to use the default directory: ");
+		String temp = scan.nextLine();
+		if(temp.isEmpty()) {
+			System.out.println("Proceeding with the default directory.");
+		} else {
+			setPath(temp);
+			System.out.println("We will use '" + temp + "' as our working directory.");
+		}
+		
+	}
+	
+	
+	private static void collectionMainMenuOption() {
+		
+		System.out.println("Please choose 1, 2 or 3:");
+        String option = scan.nextLine();
+        
+        switch (option) {
+        
+            case "1":
+                showFilesInAscendingOrder();
+                break;
+                
+            case "2":
+            	showFileOpMenu();
+            	break;
+
+            case "3":
+                System.out.println("Thanks for using lockedme.com. Closing application.");
+                System.exit(0);
+                break;
+                
+            default:
+                System.out.println("Invalid input provided, please choose 1, 2 or 3.");
+        }
+        showMainMenu();
+	}
+	
+	public static void setPath(String pathname) {
+		MY_DIR = pathname;
+		return;
+	}
+	
+	
+	private static void showFilesInAscendingOrder() {
+		System.out.println("-------------------------------------------------");
+        //System.out.println("Showing files in ascending order at:" + MY_DIR );
+       // File f = new File(MY_DIR);
+        //if(f.exists()) {
+        	//System.out.println("THIS PATH EXISTS ALREADY");
+        //} else {
+        	//System.out.println("WE JUST MADE THE DIR WE JSUT MADDE THE DIR");
+        	//f.mkdir();
+        //}
+        File[] files = new File(MY_DIR).listFiles();
+        Set<String> sorted = new TreeSet<>();
+        for (File file: files) {
+            if (!file.isFile()) {
+                continue;
+            }
+            sorted.add(file.getName());
+        }
+        sorted.forEach(System.out::println);
+        System.out.println("-------------------------------------------------");
+	}
+	
+	private static void showMainMenu() {
+		System.out.println("------ MAIN MENU -----");
+        System.out.println("1.) Show files in ascending order");
+        System.out.println("2.) Perform file operations");
+        System.out.println("3.) Close the application");
+        System.out.println("---------------------");
+        collectionMainMenuOption();
+	}
+	
+	private static void showWelcomeScreen() {
+        System.out.println("---------------------------");
+        System.out.println(" Application: LockedMe.com");
+        System.out.println("  Developer:   Anupam Deo");
+        System.out.println("---------------------------");
+        System.out.println("\n");
+	}
+	
+	private static void fileOpMenuOption() {
+		
+		System.out.println("Please choose 1, 2, 3, 4");
+        String option = scan.nextLine();
+        String filename = "";
+        int searchResult = 0;
+        
+        switch (option) {
+                
+            case "1":
+            	filename = getFileName();
+            	addFile(filename);
+            	break;
+
+            case "2":
+            	filename = getFileName();
+            	deleteFile(filename);
+                break;
+                
+            case "3":
+            	searchResult = searchFiles();
+            	switch(searchResult) {
+	            	
+            		//No match
+	            	case 0:
+	            		System.out.println("\nThere were no matches for that filename.");
+	            		break;
+	            		
+	            	//Similar string
+	            	case 1:
+	            		
+	            		break;
+	            		
+	            	//Perfect match
+	            	case 2:
+	            		System.out.println("\nYes. That files exists within the directory!");
+	            		break;
+            	
+            	}
+            	break;
+                
+            case "4":
+            	showMainMenu();
+            	break;
+                
+            default:
+                System.out.println("Invalid input provided, please choose 1, 2, 3, or 4.");
+        }
+        showFileOpMenu();
+		
+	}
+	
+	private static void showFileOpMenu() {
+		
+		System.out.println("------ File Operations Menu -----------------------");
+		System.out.println("1) Add a file");
+		System.out.println("2) Delete a file");
+		System.out.println("3) Search for a file");
+		System.out.println("4) Back to main menu");
+		System.out.println("---------------------------------------------------");
+		
+		
+		
+		
+		fileOpMenuOption();
+	}
+	
+	private static String getFileName() {
+		
+		System.out.println("Please enter a filename:");
+		String temp = scan.nextLine();
+		
+		while(temp.isEmpty()) {
+			System.out.println("Error! Please enter a valid filename:");
+			temp = scan.nextLine();
+		}
+		
+		return temp;
+	}
+	
+}
